270b067ad2a1bd1e9696b1ba71ae65084f82a07f
RandomNari <giotareddragon@gmail.com>
2026-07-21 00:21:39 +0200

    Initial commit

---
+++ src/client/java/cloudcities/random/nari/client/CloudcitiesClient.java
@@ -0,0 +1,10 @@
+package cloudcities.random.nari.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CloudcitiesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/cloudcities/random/nari/client/CloudcitiesDataGenerator.java
@@ -0,0 +1,11 @@
+package cloudcities.random.nari.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CloudcitiesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
