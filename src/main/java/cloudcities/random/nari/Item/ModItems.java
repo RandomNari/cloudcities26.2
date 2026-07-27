2655ffde11f64459d7cb34e4f3585035732f3abc
RandomNari <giotareddragon@gmail.com>
2026-07-27 03:01:58 +0200

    made cotton candy edible

--- src/main/java/cloudcities/random/nari/Item/ModItems.java
+++ src/main/java/cloudcities/random/nari/Item/ModItems.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 
@@ -19,7 +20,7 @@ public class ModItems {
 
         return item;
     }
-    public static final Item cotton_candy = register(ModItemIds.COTTON_CANDY, Item::new, new Item.Properties());
+    public static final Item cotton_candy = register(ModItemIds.COTTON_CANDY, Item::new, new Item.Properties().food(new FoodProperties.Builder().build()));
     public static void initialize() {
         CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.FOOD_AND_DRINKS)
                 .register((creativeTab) -> creativeTab.accept(ModItems.cotton_candy));
